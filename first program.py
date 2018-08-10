299272df96e41a5b3aededaf617b977aa5585e6c
MarufurRahman <marufurrahman1997@gmail.com>
2018-08-11 00:57:00 +0600

    Initial commit

---
+++ first program.py
@@ -0,0 +1,16 @@
+# This program says hello and asks for my name.
+
+print('Hello World!')
+
+print('What is your name?') # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is :')
+print(len(myName))
+
+print('What is your age?') # ask for their age
+myAge = int(input())
+print('You will be older than me because i am a bot')
+
+
+print('Good bye...')
