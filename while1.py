3ed6d71b4eff5526d48ef76fa3dae66cfbcc1890
MarufurRahman <marufurrahman1997@gmail.com>
2018-08-13 01:04:37 +0600

    Initial commit

---
+++ while1.py
@@ -0,0 +1,13 @@
+# Eat some chicken...
+# Programmed by Marufur Rahman.
+
+while 1:
+    print ('Hey,')
+    answer = input('Press y for large fried chicken ')
+    if answer == 'y':
+        print ('Large fried chicken with, mmmm, yummy ')
+        continue
+    answer = input('Had enough yet? ')
+    if answer == 'y':
+        break
+print ('Have a nice day')
