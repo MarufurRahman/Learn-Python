e1ffc29ef2c9a64a54fe46e0182881ed19adfbe5
MarufurRahman <marufurrahman1997@gmail.com>
2018-08-12 23:39:48 +0600

    Initial commit

---
+++ Iterating dictionary.py
@@ -0,0 +1,10 @@
+# Iterating dictionary
+
+marufInfo = {
+	'Full Name':'Marufur Rahman', 
+	'Class':'10', 
+	'Roll':'133', 
+	'Hometown':'Patiya'
+}
+for key in marufInfo:
+	print('{}: {}'.format(key, marufInfo[key]))
